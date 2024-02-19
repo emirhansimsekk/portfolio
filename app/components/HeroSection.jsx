caca44dfbb2d7f48f65bc378603dafa97837540e
emirhansimsekk <20120205046@ismu.edu.tr>
2024-02-19 13:32:53 +0300

    commit

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-200'>Hello, I'm{" "} </span>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-200'>Hello, Im{" "} </span>
               <TypeAnimation
                 sequence={[
                   // Same substring at the start will only be typed out once, initially

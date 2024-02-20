9b1c091f066aa4f265e3b3fc6f35ec40fffbe479
emirhansimsekk <20120205046@ismu.edu.tr>
2024-02-20 11:10:11 +0300

    commit

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -1,48 +1,102 @@
-"use client"
-import React, { useState, useTransition } from "react";
+"use client";
+import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
 
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>NextJS and React Native</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>Kotlin</li>
+        <li>Python</li>
+        <li>PostgreSQL</li>
+        <li>ArcGIS for .NET</li>
+
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Computer Engineering, İstanbul Medeniyet Üniversitesi</li>
+        <li>Prof. Dr. Nabi Avcı Anadolu Lisesi</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Experiences",
+    id: "experiences",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Ideon - Full Stack Developer </li>
+        <li>BAYKAR Teknoloji – Object Oriented Programming(ArcGIS) </li>
+        <li>Anadolubank- Mobile Programming(Android)</li>
+      </ul>
+    ),
+  },
+];
+
 const AboutSection = () => {
-  const [tab, setTab] = useState("skilss")
-  const [startTransition, isPending] = useTransition()  
-  
-  const handleTabChange = (id) =>{
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
     startTransition(() => {
-        setTab(id);
-      }
-    )
-  }
-  return(
-   <section className="text-white">
-    <div className="md:grid md:grid-cols-2 gap-0 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <Image src="/images/about.jpg" width={400} height={200}/>
-      <div>
-        <h2 className="text-4xl font-bold text-white mb-4">
-          About Me
-        </h2>
-        <p className="text-base lg:text-lg">
-            I am a full stack mobile developer. I have experience
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about.jpg" width={500} height={500} className="rounded-full" />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack mobile developer with a passion for creating
+            interactive and responsive web applications. I have experience
             working with JavaScript, React, Node.js, Express, PostgreSQL,
             Java, Kotlin, Python and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
-        </p>  
-        <div className="flex flex-row mt-8">
-          <TabButton selectTab={() => handleTabChange("skills")} active={tab==="skills"}>
-            {" "}
-            Skills{" "}  
-          </TabButton>  
-          <span className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-blue-400">Skills</span>
-          <span className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-blue-400">Skills</span>
-          <span >Education</span>
-          <span>Experience</span>
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("experiences")}
+              active={tab === "experiences"}
+            >
+              {" "}
+              Experiences{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
         </div>
       </div>
-    </div>
-   </section>
-  ) 
-
+    </section>
+  );
 };
 
 export default AboutSection;
